dde7105ab84714ea6a2ec92574ade9f31afe6813
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2016-10-01 20:52:37 +0200

    Implement a decorator to make decorator easily

---
+++ pyhooks/utils.py
@@ -0,0 +1,6 @@
+def decorator_with_args(decorator_to_enhance):
+    def decorator_maker(*args, **kwargs):
+        def decorator_wrapper(func):
+            return decorator_to_enhance(func, *args, **kwargs)
+        return decorator_wrapper
+    return decorator_maker

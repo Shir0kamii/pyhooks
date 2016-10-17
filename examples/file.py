cd44345d43e2d3a5dc9ebb222895fdb75568878e
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2016-10-17 19:51:28 +0200

    Add an example about files

---
+++ examples/file.py
@@ -0,0 +1,22 @@
+from pyhooks import precall_register, postcall_register, Hook
+class FileManager(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    @Hook
+    def write(self, text):
+        self.file.write(text)
+
+
+class TextfileManager(FileManager):
+
+    @precall_register('write')
+    def open_file(self, *args):
+        self.file = open(self.filename, 'w')
+
+    @postcall_register('write')
+    def close_file(self, *args):
+        self.file.close()
+
+TextfileManager("testing.txt").write("This is a test.")

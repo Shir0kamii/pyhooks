3f3380c9cbc2b1782cfe5d619772d83659ed40e9
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2016-10-18 20:36:00 +0200

    Add an example with logging

---
+++ examples/heavy_computation.py
@@ -0,0 +1,25 @@
+from pyhooks import precall_register, postcall_register, Hook
+import logging
+import time
+
+
+class HeavyComputation(object):
+
+    @precall_register('heavy_computation')
+    def begin_computation(self, *args, **kwargs):
+        print("The ongoing operation could take some time to complete")
+
+    @postcall_register('heavy_computation')
+    def end_computation(self, *args, **kwargs):
+        print("The running operation has ended")
+
+
+class LifeQuestion(HeavyComputation):
+
+    @Hook
+    def heavy_computation(self):
+        time.sleep(2)
+        return 42
+
+computer = LifeQuestion()
+print(computer.heavy_computation())

516b352175ce0840f839432f79c9ff7f702dfb5b
Michael Shustin <michaelshustin@gmail.com>
2019-10-14 13:39:16 +0300

    Updated PyPI classifiers

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     platforms="any",
     packages=find_packages(),
     classifiers=[
+        "Development Status :: 5 - Production/Stable"
+        "Operating System :: Microsoft :: Windows"
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
@@ -32,6 +34,9 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ]
 )
